7f369dbbfe169cc32f474da0a8389910a0455ff3
Git-Project01 <git_project1@qq.com>
2020-04-29 11:16:58 +0800

    修改Student.java 添加StudentDao.java

---
+++ src/main/java/com/imti/dao/StudentDao.java
@@ -0,0 +1,9 @@
+package com.imti.dao;
+
+import com.imti.pojo.Student;
+
+public class StudentDao {
+	public void insertStudent(Student student) {
+		System.out.println("insert Student" + student);
+	}
+}
--- src/main/java/com/imti/pojo/Student.java
+++ src/main/java/com/imti/pojo/Student.java
@@ -2,9 +2,19 @@ package com.imti.pojo;
 
 public class Student {
 
-	private String name;					
-	//edit by Git-Project02
-	//edit by Git-Project01
+	private String name;
+	// edit by Git-Project02
+	// edit by Git-Project01
+
+	private Integer age;
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
 
 	public String getName() {
 		return name;
